7d7bf8380bfd743743367f7bb39a454c21dbfef5
Kareem795 <kemoyasser2003@gmail.com>
2026-02-01 03:04:03 +0200

    fix(navigation): replace context.go with context.push

--- lib/features/profile/presentation/view/pages/profile_page.dart
+++ lib/features/profile/presentation/view/pages/profile_page.dart
@@ -314,7 +314,7 @@ class _ProfilePageState extends State<ProfilePage> {
                       iconPath: AppIcons.iconsWarning,
                       title: LocaleKeys.profile_terms_conditions.tr(),
                       onTap: () {
-                        context.go(Routes.termsAndConditions);
+                        context.push(Routes.termsAndConditions);
                       },
                     ),
 
