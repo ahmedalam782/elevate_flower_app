16d403b605580a158343171b5dbd5ca3d8c3d5b1
khaled Ahmed <khaledahm79@gmail.com>
2026-03-01 05:24:42 +0200

    fix cart bug in home

--- lib/features/home/presentation/view/widgets/header_section.dart
+++ lib/features/home/presentation/view/widgets/header_section.dart
@@ -1,9 +1,7 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
 
 import '../../../../../core/languages/locale_keys.g.dart';
-import '../../../../../core/routes/routes.dart';
 import '../../../../../core/theme/app_colors.dart';
 import '../../../../../core/theme/app_images.dart';
 import 'search_text_field.dart';
@@ -21,15 +19,10 @@ class HeaderSection extends StatelessWidget {
           Row(
             children: [
               // Logo
-              InkWell(
-                onTap: () {
-                  context.push(Routes.carScreen);
-                },
-                child: Image.asset(
-                  AppImages.imagesIcLauncherAndroid,
-                  width: 30,
-                  height: 30,
-                ),
+              Image.asset(
+                AppImages.imagesIcLauncherAndroid,
+                width: 30,
+                height: 30,
               ),
               const SizedBox(width: 8),
               Text(
