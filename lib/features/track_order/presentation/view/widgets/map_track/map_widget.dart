f985be2e1237d88bcf2609d83fc4a3d3d858d0fb
khaled Ahmed <khaledahm79@gmail.com>
2026-03-04 14:08:19 +0200

    feat: Enhance map widget with zoom functionality and markers
    - Increased initial zoom level of the map for better visibility.
    - Disabled zoom controls and gestures for a more controlled user experience.
    - Added two markers to the map for specific locations.
    - Implemented zoomToFitTwoPoints function to adjust camera bounds based on marker positions.

--- lib/features/track_order/presentation/view/widgets/map_track/map_widget.dart
+++ lib/features/track_order/presentation/view/widgets/map_track/map_widget.dart
@@ -13,11 +13,12 @@ class MapWidget extends StatefulWidget {
 }
 
 class _MapWidgetState extends State<MapWidget> {
+  LatLngBounds? bounds;
   final Completer<GoogleMapController> _controller =
       Completer<GoogleMapController>();
   static const CameraPosition _kGooglePlex = CameraPosition(
     target: LatLng(30.964060450497563, 31.233796378712483),
-    zoom: 17,
+    zoom: 19,
   );
   String? _style;
   @override
@@ -37,10 +38,33 @@ class _MapWidgetState extends State<MapWidget> {
         : Expanded(
             child: GoogleMap(
               initialCameraPosition: _kGooglePlex,
+              zoomControlsEnabled: false,
+              zoomGesturesEnabled: false,
+              markers: {
+                const Marker(
+                  markerId: MarkerId("1"),
+                  position: LatLng(31.064474265366416, 31.433740570489253),
+                ),
+                const Marker(
+                  markerId: MarkerId("2"),
+                  position: LatLng(30.966074265366416, 31.236740570489253),
+                ),
+              },
+
               style: _style,
               onMapCreated: (GoogleMapController controller) async {
                 _controller.complete(controller);
+                if (bounds == null) {
+                  bounds = await zoomToFitTwoPoints(
+                    controller,
+                    const LatLng(31.064474265366416, 31.433740570489253),
+                    const LatLng(30.966074265366416, 31.236740570489253),
+                  );
+                  setState(() {});
+                }
+                // controller.animateCamera(CameraUpdate.newLatLngBounds());
               },
+              cameraTargetBounds: CameraTargetBounds(bounds),
             ),
           );
   }
@@ -52,4 +76,34 @@ Future<String?> _getMapStyle({required String path}) async {
   } catch (_) {
     return null;
   }
+}
+
+Future<LatLngBounds> zoomToFitTwoPoints(
+  GoogleMapController controller,
+  LatLng point1,
+  LatLng point2,
+) async {
+  LatLngBounds bounds;
+
+  if (point1.latitude > point2.latitude &&
+      point1.longitude > point2.longitude) {
+    bounds = LatLngBounds(southwest: point2, northeast: point1);
+  } else if (point1.longitude > point2.longitude) {
+    bounds = LatLngBounds(
+      southwest: LatLng(point1.latitude, point2.longitude),
+      northeast: LatLng(point2.latitude, point1.longitude),
+    );
+  } else if (point1.latitude > point2.latitude) {
+    bounds = LatLngBounds(
+      southwest: LatLng(point2.latitude, point1.longitude),
+      northeast: LatLng(point1.latitude, point2.longitude),
+    );
+  } else {
+    bounds = LatLngBounds(southwest: point1, northeast: point2);
+  }
+
+  CameraUpdate cameraUpdate = CameraUpdate.newLatLngBounds(bounds, 50);
+
+  controller.animateCamera(cameraUpdate);
+  return bounds;
 }
